939be3e4c0007e647faf0ff19612e644e1b8f194
amira2011 <shaikh.abidali@gmail.com>
2024-03-28 01:15:08 +0530

    CSS

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,7 +1,5 @@
-class Genre < ApplicationRecord 
-
-    has_many :book_genres 
-    has_many :books , :through => :book_genres
-
-    
+class Genre < ApplicationRecord
+  has_many :book_genres
+  has_many :books, :through => :book_genres
+  chars = "a,c,v"
 end
