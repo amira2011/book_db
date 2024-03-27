475054a0841b544ff57cc2aef1c91be52c034b39
amira2011 <shaikh.abidali@gmail.com>
2024-03-27 21:07:41 +0530

    ransackable_attributes

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,11 +1,16 @@
 class Book < ApplicationRecord
-  has_one :publisher
   belongs_to :publisher
   has_many :book_authors, dependent: :destroy
   has_many :authors, :through => :book_authors
   has_many :book_genres, dependent: :destroy
   has_many :genres, :through => :book_genres
   after_create :save_json
+
+  scope :recent, ->(limit = 5) { order(created_at: :desc).limit(limit) }
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "id", "id_value", "isbn", "published_date", "publisher_id", "rating", "title", "total_pages", "updated_at"]
+  end
 
   def save_json
     puts "We are storing data to JSON"
@@ -19,16 +24,5 @@ class Book < ApplicationRecord
     #data.push(self.authors.as_json)
     #data.push(self.genres.as_json)
     Utils.post_data(data)
-  end
-
-  def self.ransackable_associations(auth_object = nil)
-    ["authors", "book_authors", "book_genres", "genres", "publisher"]
-  end
-
-  def self.ransackable_attributes(auth_object = nil)
-    # List the attributes you want to be searchable. Exclude any sensitive information.
-    # The list provided in the error message is a good starting point, but let's assume 'id_value' might be sensitive or irrelevant.
-    # Adjust the list according to your application's needs.
-    ["created_at", "id", "isbn", "published_date", "publisher_id", "rating", "title", "total_pages", "updated_at"]
   end
 end
