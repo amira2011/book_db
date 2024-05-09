0b3ff019bb4e0d9120f750bab7aab89aa9800eef
amira2011 <shaikh.abidali@gmail.com>
2024-05-09 23:11:25 +0530

    Authenticate Admin User

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,59 +1,55 @@
 class AuthorsController < ApplicationController
+  before_action :authenticate_admin_user!
 
-    def index
-        @authors = Author.all
+  def index
+    @authors = Author.all
+  end
+
+  def show
+    @author = Author.find(params[:id])
+    @books = @author.books
+  end
+
+  def new
+    @author = Author.new
+  end
+
+  def delete
+  end
+
+  def destroy
+    @author = Author.find(params[:id])
+    if @author.destroy
+      redirect_to authors_path
     end
-    
-    def show
-        @author = Author.find(params[:id])
-        @books = @author.books
+  end
+
+  def edit
+    @author = Author.find(params[:id])
+  end
+
+  def update
+    @author = Author.find(params[:id])
+    if @author.update(author_params)
+      redirect_to author_path(@author)
+    else
+      render "edit"
     end
+  end
 
-    def new
-        @author = Author.new
+  def create
+    @author = Author.new(author_params)
+    if @author.save
+      flash[:success] = "Successfully Created"
+      redirect_to authors_path
+    else
+      render "new", status: :unprocessable_entity
     end
+  end
 
-    def delete
-    
-    end
-    
-    
-    def destroy
-        @author= Author.find(params[:id])
-        if  @author.destroy
-            redirect_to authors_path
-        end
-    end
+  private
 
-
-    def edit
-        @author = Author.find(params[:id])
-    end
-
-    def update
-        @author = Author.find(params[:id])
-        if @author.update(author_params)
-            redirect_to author_path(@author)
-        else 
-            render 'edit'
-        end
-    end
-
-    def create
-        @author = Author.new(author_params)
-        if  @author.save 
-            flash[:success]="Successfully Created"
-            redirect_to authors_path
-        else
-            render 'new', status: :unprocessable_entity
-        end   
-    end
-
-    private
-
-    def author_params
-        params.require(:author).permit(:first_name, :middle_name, :last_name)
-    end
-    
-
+  def author_params
+    params.require(:author).permit(:first_name, :middle_name, :last_name)
+  end
 end
